8fe7c8de24c65be423c82412e8b0270ae1dacadf
Bhargav Dakwala <54886489+bhargavdakwala@users.noreply.github.com>
2023-02-13 19:40:23 +0100

    Add files via upload
    client application

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.Sending = new System.Windows.Forms.Label();
+            this.gvSending = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gvReceived = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSending)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvReceived)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRefresh);
+            this.groupBox1.Controls.Add(this.btnSend);
+            this.groupBox1.Controls.Add(this.txtSend);
+            this.groupBox1.Controls.Add(this.Sending);
+            this.groupBox1.Location = new System.Drawing.Point(100, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(517, 133);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client Form";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(121, 81);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(16, 82);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Sending";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(111, 39);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(302, 23);
+            this.txtSend.TabIndex = 1;
+            // 
+            // Sending
+            // 
+            this.Sending.AutoSize = true;
+            this.Sending.Location = new System.Drawing.Point(16, 39);
+            this.Sending.Name = "Sending";
+            this.Sending.Size = new System.Drawing.Size(38, 15);
+            this.Sending.TabIndex = 0;
+            this.Sending.Text = "label1";
+            // 
+            // gvSending
+            // 
+            this.gvSending.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvSending.Location = new System.Drawing.Point(13, 27);
+            this.gvSending.Name = "gvSending";
+            this.gvSending.Size = new System.Drawing.Size(312, 150);
+            this.gvSending.TabIndex = 1;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            // 
+            // gvReceived
+            // 
+            this.gvReceived.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvReceived.Location = new System.Drawing.Point(26, 22);
+            this.gvReceived.Name = "gvReceived";
+            this.gvReceived.RowTemplate.Height = 25;
+            this.gvReceived.Size = new System.Drawing.Size(338, 150);
+            this.gvReceived.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.gvSending);
+            this.groupBox2.Location = new System.Drawing.Point(12, 235);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(351, 203);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sending";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.gvReceived);
+            this.groupBox3.Location = new System.Drawing.Point(381, 235);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(388, 219);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Receved";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSending)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvReceived)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Label Sending;
+        private System.Windows.Forms.DataGridView gvSending;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InputOutput;
+        private System.Windows.Forms.DataGridView gvReceived;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Received;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ client/client/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Data.SQLite;
+using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "127.0.0.1";
+        public SQLiteConnection mySQLiteConnection = new SQLiteConnection("Data Source=E:/Sample/client/SendingData.sqlite3");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ReceivedData();
+        }
+
+
+        private void DataSending(string datasend)
+        {
+            try
+            {
+                //---data to send to the server---
+
+                string textToSend = datasend;
+
+                //---create a TCPClient object at the IP and port no.---
+                TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+                NetworkStream nwStream = client.GetStream();
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+                //---send the text---
+                Console.WriteLine("Sending : " + textToSend);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                //---read back the text---
+                //byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                //int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+
+                //Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                // Console.ReadLine();
+                client.Close();
+                InsertSendingData(datasend);
+                BindGridSending();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ReceivedData()
+        {
+            try
+            {
+
+                IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+                TcpListener listener = new TcpListener(localAdd, PORT_NO);
+                Console.WriteLine("Listening...");
+
+                listener.Start();
+                Console.WriteLine("started...");
+                //---incoming client connected---
+                TcpClient client = listener.AcceptTcpClient();
+
+                //---get the incoming data through a network stream---
+                NetworkStream nwStream = client.GetStream();
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                //---read incoming stream---
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //---convert the data received into a string---
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received : " + dataReceived);
+
+                //RecievedData_addGrid(dataReceived);
+
+                //---write back the text to the client---
+                //Console.WriteLine("Sending back : " + dataReceived);
+                //nwStream.Write(buffer, 0, bytesRead);
+                client.Close();
+                listener.Stop();
+                Console.ReadLine();
+                InsertReceivedData(dataReceived);
+                BindGridReceived();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            DataSending(txtSend.Text);
+
+        }
+
+        private void BindGridSending()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                mySQLiteConnection.Open();
+                SQLiteCommand cmd = new SQLiteCommand("select * from SendingData  where Status='sent'", mySQLiteConnection);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+                da.Fill(dt);
+                mySQLiteConnection.Close();
+
+                gvSending.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void BindGridReceived()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                mySQLiteConnection.Open();
+                SQLiteCommand cmd = new SQLiteCommand("select * from SendingData  where Status='Received'", mySQLiteConnection);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+                da.Fill(dt);
+                mySQLiteConnection.Close();
+
+                gvReceived.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void InsertReceivedData(string dataReceived)
+        {
+            mySQLiteConnection = new SQLiteConnection("Data Source=E:/Sample/client/SendingData.sqlite3");
+            if (!File.Exists("E:/Sample/client/SendingData.sqlite3"))
+            {
+
+                SQLiteConnection.CreateFile("E:/Sample/client/SendingData.sqlite3");
+                Console.WriteLine("Wrestler Database file created");
+            }
+            try
+            {
+
+                SQLiteCommand cmd = new SQLiteCommand(mySQLiteConnection);
+                cmd = mySQLiteConnection.CreateCommand();
+                mySQLiteConnection.Open();
+
+
+                //cmd.CommandText = "DROP TABLE IF EXISTS RecivedData";
+                //cmd.ExecuteNonQuery();
+
+                //cmd.CommandText = @"CREATE TABLE RecivedData(id INTEGER PRIMARY KEY,
+                //data TEXT, datetime TEXT,status TEXT)";
+                //cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "INSERT INTO SendingData(Data, Datetime,Status) VALUES('" + dataReceived + "','" + DateTime.Now + "','Received')";
+                cmd.ExecuteNonQuery();
+
+                mySQLiteConnection.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                mySQLiteConnection.Close();
+            }
+
+
+        }
+        public void InsertSendingData(string data)
+        {
+            mySQLiteConnection = new SQLiteConnection("Data Source=E:/Sample/client/SendingData.sqlite3");
+            if (!File.Exists("E:/Sample/client/SendingData.sqlite3"))
+            {
+
+                SQLiteConnection.CreateFile("E:/Sample/client/SendingData.sqlite3");
+                Console.WriteLine("Wrestler Database file created");
+            }
+            try
+            {
+
+                SQLiteCommand cmd = new SQLiteCommand(mySQLiteConnection);
+                cmd = mySQLiteConnection.CreateCommand();
+                mySQLiteConnection.Open();
+
+
+                //cmd.CommandText = "DROP TABLE IF EXISTS SendingData";
+                //cmd.ExecuteNonQuery();
+
+                //cmd.CommandText = @"CREATE TABLE SendingData(id INTEGER PRIMARY KEY,
+                //data TEXT, datetime TEXT,status TEXT)";
+                //cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "INSERT INTO SendingData(Data, Datetime,Status) VALUES('" + data + "','" + DateTime.Now + "','sent')";
+                cmd.ExecuteNonQuery();
+
+                mySQLiteConnection.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                mySQLiteConnection.Close();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            BindGridSending();
+            BindGridReceived();
+            ReceivedData();
+        }
+    }
+}
